8accab749c665c8944032e76d5b086bc2ee90f9e
hantek <lin.zhouhan@gmail.com>
2015-04-04 13:03:50 -0400

    First running script, 87.96% accuracy.

---
+++ expr_catsndogs_CONV.py
@@ -0,0 +1,213 @@
+import os
+import sys
+import numpy
+import theano
+import theano.tensor as T
+from theano.tensor.shared_randomstreams import RandomStreams
+
+from layer import ReluConv2DLayer, MaxPoolingLayer, ReluLayer, LinearLayer
+from classifier import LogisticRegression
+from train import GraddescentMinibatch, Dropout
+from params import save_params, load_params, set_params, get_params
+
+import pdb
+
+
+#######################
+# SET SUPER PARAMETER #
+#######################
+
+batchsize = 100
+momentum = 0.9
+
+weightdecay = 0.01
+finetune_lr = 5e-3
+finetune_epc = 400
+
+print " "
+print "batchsize =", batchsize
+print "momentum =", momentum
+print "finetune:            lr = %f, epc = %d" % (finetune_lr, finetune_epc)
+
+#############
+# LOAD DATA #
+#############
+
+print "... preparing data"
+class data_generator(object):
+    def __init__(self, string, truthstring, numpart=8):
+        self.string = string
+        self.truthstring = truthstring
+        self.partidx = 0
+        self.numpart = numpart
+    
+    def __iter__(self):
+        return self
+
+    def next(self):
+        if self.partidx < self.numpart:
+            filename = self.string + '%d.npy' % self.partidx
+            truthname = self.truthstring + '%d.npy' % self.partidx
+            data = numpy.load(filename)
+            truth = numpy.load(truthname)
+            self.partidx += 1
+            return (data, truth)
+        else:
+            self.partidx = 0
+            raise StopIteration
+    
+    def reset(self):
+        self.partidx = 0
+
+
+train_dg = data_generator('train_padnormbc01_part', 'train_truth_part', 8)
+test_dg = data_generator('valid_padnormbc01_part', 'valid_truth_part', 2)
+
+x, y = train_dg.next()
+train_dg.reset()
+train_x = theano.shared(value=x, name='train_x', borrow=True)
+train_y = theano.shared(value=y, name='train_y', borrow=True)
+x, y = test_dg.next()
+test_dg.reset()
+test_x = theano.shared(value=x, name='test_x', borrow=True)
+test_y = theano.shared(value=y, name='test_y', borrow=True)
+print "Done."
+
+###############
+# BUILD MODEL #
+###############
+
+print "... building model"
+l0_n_in = (batchsize, 3, 250, 250)
+l0_filter_shape=(32, l0_n_in[1], 11, 11)
+l0_pool_n_in = (batchsize, l0_filter_shape[0], l0_n_in[2]-l0_filter_shape[2]+1, l0_n_in[3]-l0_filter_shape[3]+1)
+l0_poolsize = (3, 3)
+
+l1_n_in = (batchsize, l0_filter_shape[0], l0_pool_n_in[2]/l0_poolsize[0], l0_pool_n_in[3]/l0_poolsize[1])
+l1_filter_shape=(48, l1_n_in[1], 5, 5)
+l1_pool_n_in = (batchsize, l1_filter_shape[0], l1_n_in[2]-l1_filter_shape[2]+1, l1_n_in[3]-l1_filter_shape[3]+1)
+l1_poolsize = (3, 3)
+
+l2_n_in = (batchsize, l1_filter_shape[0], l1_pool_n_in[2]/l1_poolsize[0], l1_pool_n_in[3]/l1_poolsize[1])
+l2_filter_shape=(64, l2_n_in[1], 3, 3)
+l2_pool_n_in = (batchsize, l2_filter_shape[0], l2_n_in[2]-l2_filter_shape[2]+1, l2_n_in[3]-l2_filter_shape[3]+1)
+l2_poolsize = (3, 3)
+
+l3_n_in = (batchsize, l2_filter_shape[0], l2_pool_n_in[2]/l2_poolsize[0], l2_pool_n_in[3]/l2_poolsize[1])
+l3_filter_shape=(128, l3_n_in[1], 3, 3)
+l3_pool_n_in = (batchsize, l3_filter_shape[0], l3_n_in[2]-l3_filter_shape[2]+1, l3_n_in[3]-l3_filter_shape[3]+1)
+l3_poolsize = (2, 2)
+
+npy_rng = numpy.random.RandomState(123)
+model = ReluConv2DLayer(
+    init_w=None,
+    n_in=l0_n_in, filter_shape=l0_filter_shape, npy_rng=npy_rng
+) + MaxPoolingLayer(
+    n_in=l0_pool_n_in, pool_size=l0_poolsize, ignore_border=True
+) + ReluConv2DLayer(
+    init_w=None,
+    n_in=l1_n_in, filter_shape=l1_filter_shape, npy_rng=npy_rng
+) + MaxPoolingLayer(
+    n_in=l1_pool_n_in, pool_size=l1_poolsize, ignore_border=True
+) + ReluConv2DLayer(
+    init_w=None,
+    n_in=l2_n_in, filter_shape=l2_filter_shape, npy_rng=npy_rng
+) + MaxPoolingLayer(
+    n_in=l2_pool_n_in, pool_size=l2_poolsize, ignore_border=True
+) + ReluConv2DLayer(
+    init_w=None,
+    n_in=l3_n_in, filter_shape=l3_filter_shape, npy_rng=npy_rng
+) + MaxPoolingLayer(
+    n_in=l3_pool_n_in, pool_size=l3_poolsize, ignore_border=True
+) + LinearLayer(512, 128, npy_rng=npy_rng
+) + ReluLayer(128, 512, npy_rng=npy_rng
+) + LinearLayer(512, 128, npy_rng=npy_rng
+) + LogisticRegression(128, 10, npy_rng=npy_rng)
+
+model.print_layer()
+# load_params(model, 'CONV_apr_4.npy')
+
+# compile error rate counters:
+index = T.lscalar()
+truth = T.lvector('truth')
+train_set_error_rate = theano.function(
+    [index],
+    T.mean(T.neq(model.models_stack[-1].predict(), truth)),
+    givens = {model.varin : train_x[index * batchsize: (index + 1) * batchsize],
+              truth : train_y[index * batchsize: (index + 1) * batchsize]},
+)
+def train_error():
+    total_mean = 0.
+    mem = train_dg.partidx
+    train_dg.reset()
+    for ipart in train_dg:
+        train_x.set_value(ipart[0])
+        train_y.set_value(ipart[1])
+        total_mean += numpy.mean([train_set_error_rate(i) for i in xrange(25)])
+    train_dg.partidx = mem
+    return total_mean / 8.
+
+test_set_error_rate = theano.function(
+    [index],
+    T.mean(T.neq(model.models_stack[-1].predict(), truth)),
+    givens = {model.varin : test_x[index * batchsize: (index + 1) * batchsize],
+              truth : test_y[index * batchsize: (index + 1) * batchsize]},
+)
+def test_error():
+    total_mean = 0.
+    mem = test_dg.partidx
+    test_dg.reset()
+    for ipart in test_dg:
+        test_x.set_value(ipart[0])
+        test_y.set_value(ipart[1])
+        total_mean += numpy.mean([test_set_error_rate(i) for i in xrange(25)])
+    test_dg.partidx = mem
+    return total_mean / 2.
+print "Done."
+print "Initial error rate: train: %f, test: %f" % (train_error(), test_error())
+
+#############
+# FINE-TUNE #
+#############
+
+print "\n\n... fine-tuning the whole network"
+trainer = GraddescentMinibatch(
+    varin=model.varin, data=train_x, 
+    truth=model.models_stack[-1].vartruth, truth_data=train_y,
+    supervised=True,
+    cost=model.models_stack[-1].cost(), 
+    params=model.params,
+    batchsize=batchsize, learningrate=finetune_lr, momentum=momentum,
+    rng=npy_rng
+)
+
+init_lr = trainer.learningrate
+prev_cost = numpy.inf
+for epoch in xrange(finetune_epc):
+    i = 0
+    cost = 0.
+    for ipart in train_dg:
+        print "part %d " % i,
+        train_x.set_value(ipart[0])
+        train_y.set_value(ipart[1])
+        i += 1
+        cost += trainer.step()
+        print "       ",
+        cost += trainer.step()
+    cost /= i * 2. 
+    if prev_cost <= cost:
+        if trainer.learningrate < (init_lr * 1e-7):
+            break
+        trainer.set_learningrate(trainer.learningrate*0.8)
+    prev_cost = cost
+    print "*** epoch %d cost: %f" % (epoch, cost)
+    print "*** error rate: train: %f, test: %f" % (train_error(), test_error())
+    try:
+        if epoch % 30 == 0:
+            save_params(model, 'CONV_11-5-3-3_32-48-64-128_3332_512-128-512-128-10.npy')
+    except:
+        pass
+print "***FINAL error rate: train: %f, test: %f" % (train_error(), test_error())
+print "Done."
+
+pdb.set_trace()
--- preprocessing.py
+++ preprocessing.py
@@ -5,11 +5,12 @@ import theano.tensor as T
 from skimage.transform import downscale_local_mean as downscale
 
 from layer import ReluConv2DLayer, MaxPoolingLayer, SigmoidLayer
-from preprocess import ZCA
+from preprocess import ZCA, SubtractMeanAndNormalize
 from dataset import CatsnDogs
 
 import pdb
 
+# set theano.config.floatX=float32 to save space!
 
 def Padding(images, padsize=(500, 500, 3), pool=2):
     """
@@ -137,11 +138,14 @@ def Padding(images, padsize=(500, 500, 3), pool=2):
 # LOAD DATA #
 #############
 
+print "... loading data"
 npy_rng = numpy.random.RandomState(123)
-data_wrapper = CatsnDogs(npy_rng=npy_rng)
 
+"""
+data_wrapper = CatsnDogs(npy_rng=npy_rng)
 train_parts = data_wrapper.train_generator()
 valid_parts = data_wrapper.valid_generator()
+"""
 
 #################
 # PAD/POOL DATA #
@@ -154,8 +158,23 @@ for ipart in train_parts:
     part_data = ipart[0]
     part_truth = ipart[1]
     paded_part = Padding(part_data)
-    filename = 'train_data_paded_part%d.npz' % i
-    numpy.savez(filename, paded_part=paded_part, part_truth=part_truth)
+    filename_data = 'train_data_paded_part%d.npy' % i
+    filename_truth = 'train_truth_part%d.npy' % i
+    numpy.save(filename_data, paded_part)
+    numpy.save(filename_truth, part_truth)
+    i += 1
+    print ".",
+    sys.stdout.flush()
+
+i = 0
+for ipart in valid_parts:
+    part_data = ipart[0]
+    part_truth = ipart[1]
+    paded_part = Padding(part_data)
+    filename_data = 'valid_data_paded_part%d.npy' % i
+    filename_truth = 'valid_truth_part%d.npy' % i
+    numpy.save(filename_data, paded_part)
+    numpy.save(filename_truth, part_truth)
     i += 1
     print ".",
     sys.stdout.flush()
@@ -165,35 +184,118 @@ for ipart in train_parts:
 # ZCA #
 #######
 
+"""
 class data_generator(object):
-    def __init__(self, string):
+    def __init__(self, string, numpart=8):
         self.string = string
         self.partidx = 0
+        self.numpart = numpart
     
     def __iter__(self):
         return self
 
     def next(self):
-        if self.partidx < 15:
-            filename = self.string + '%d.npz' % self.partidx
-            pdb.set_trace()
-            with numpy.load(filename) as part_data:
-                return part_data['paded_part']
+        if self.partidx < self.numpart:
+            filename = self.string + '%d.npy' % self.partidx
+            data = numpy.load(filename)
+            self.partidx += 1
+            return data.reshape(data.shape[0], numpy.prod(data.shape[1:]))
         else:
+            self.partidx = 0
             raise StopIteration
 
-train_dg = data_generator('train_data_paded_part')
+train_dg = data_generator('train_data_paded_part', 8)
 zca_obj = ZCA()
 zca_obj.fit_partwise(train_dg, ncases=25000, ndim=250*250*3, retain=0.99, whiten=True)
 
-for i in range(15):
-    filename = 'train_data_paded_part' + '%d.npz' % i
-    writename = 'train_padzca_part' + '%d.npz' % i
-    with numpy.load(filename) as part_data:
-        paded_data = part_data['paded_part']
-        paded_truth = part_data['part_truth']
-        paded_data_shape = paded_data.shape
-        zcaed_data = zca_obj.forward(paded_data.reshape(paded_data_shape[0], -1))
-        numpy.savez(writename, padzca_part=zcaed_data, part_truth=paded_truth)
+for i in range(8):
+    paded_data = numpy.load('train_data_paded_part%d.npy' % i)
+    zcaed_data = zca_obj.forward(paded_data.reshape(paded_data_shape[0], -1))
+    writename = 'train_padzca_part%d.npy' % i
+    numpy.save(writename, zcaed_data)
 
-pdb.set_trace()
+for i in range(2):
+    paded_data = numpy.load('valid_data_paded_part%d.npy' % i)
+    zcaed_data = zca_obj.forward(paded_data.reshape(paded_data_shape[0], -1))
+    writename = 'valid_padzca_part%d.npy' % i
+    numpy.save(writename, zcaed_data)
+"""
+"""
+model = SubtractMeanAndNormalize(250*250*3)
+map_fun = theano.function([model.varin], model.output())
+
+print "Norm over dim 1, 10 dots:"
+for i in range(8):
+    print '.', 
+    sys.stdout.flush()
+    paded_data = numpy.load('train_data_paded_part%d.npy' % i)
+    norm1_data = map_fun(paded_data.reshape(paded_data.shape[0], -1))
+    writename = 'train_padnorm1_part%d.npy' % i
+    numpy.save(writename, norm1_data)
+
+for i in range(2):
+    print '.', 
+    sys.stdout.flush()
+    paded_data = numpy.load('valid_data_paded_part%d.npy' % i)
+    norm1_data = map_fun(paded_data.reshape(paded_data.shape[0], -1))
+    writename = 'valid_padnorm1_part%d.npy' % i
+    numpy.save(writename, norm1_data)
+
+
+print "\nfinding mean and variance over dimensions, 8 dots: "
+norm = numpy.zeros((250*250*3), dtype='float32')
+variance = numpy.zeros((250*250*3), dtype='float32')
+for i in range(8):
+    print '.', 
+    sys.stdout.flush()
+    paded_data = numpy.load('train_padnorm1_part%d.npy' % i)
+    norm += paded_data.mean(0) / 8.0
+    variance += paded_data.var(0) / 8.0
+
+stds = numpy.sqrt(variance)
+
+print "extracting mean "
+for i in range(8):
+    print '.', 
+    sys.stdout.flush()
+    paded_data = numpy.load('train_padnorm1_part%d.npy' % i)
+    paded_data -= norm
+    paded_data /= stds
+    writename = 'train_padnorm_part%d.npy' % i
+    numpy.save(writename, paded_data.reshape(2500, 250, 250, 3))
+
+for i in range(2):
+    print '.', 
+    sys.stdout.flush()
+    paded_data = numpy.load('valid_padnorm1_part%d.npy' % i)
+    paded_data -= norm
+    paded_data /= stds
+    writename = 'valid_padnorm_part%d.npy' % i
+    numpy.save(writename, paded_data.reshape(2500, 250, 250, 3))
+"""
+
+
+
+print "swaping axes "
+for i in range(8):
+    print '.', 
+    sys.stdout.flush()
+    data = numpy.load('train_padnorm_part%d.npy' % i)
+    data = numpy.swapaxes(data, 1, 3)
+    data = numpy.swapaxes(data, 2, 3)
+    writename = 'train_padnormbc01_part%d.npy' % i
+    numpy.save(writename, data)
+
+for i in range(2):
+    print '.', 
+    sys.stdout.flush()
+    data = numpy.load('valid_padnorm_part%d.npy' % i)
+    data = numpy.swapaxes(data, 1, 3)
+    data = numpy.swapaxes(data, 2, 3)
+    writename = 'valid_padnormbc01_part%d.npy' % i
+    numpy.save(writename, data)
+
+
+
+
+
